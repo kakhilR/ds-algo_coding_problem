bc7d07a65a18e9e6e0f2341aaba9ec3db32b91da
kakhilR <akhilkarenae@gmail.com>
2022-07-26 00:46:04 +0530

    find frequency in ranges

---
+++ findFreqGivenRanges.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class findFreqGivenRanges {
+    public static void findFrequencyEle(int[] A){
+        HashMap<Integer, ArrayList<Integer>> myMap = new HashMap<Integer, ArrayList<Integer>>();
+        for(int i=0;i<A.length;i++){
+            if(myMap.containsKey(A[i])){
+                myMap.get(A[i]).add(i);
+            }
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(i);
+            myMap.put(A[i],list);
+        }
+        System.out.println(myMap.size());
+        
+        
+    }
+	public static void main(String[] args) {
+	    int[] A = {2,8,6,9,8,6,8,2,11,3,3};
+	    findFrequencyEle(A);
+		System.out.println("Hello World");
+	}
+    
+}
